57885cacafd95c1489056ccfe9bfc6160d7bbe43
Soxxxer <peerapat.ta@kkumail.com>
2025-10-01 11:49:41 +0700

    tower attack

--- goof_around/scenes/ui.gd
+++
@@ -1,8 +0,0 @@
-extends CanvasLayer
-class_name UI
-
-func _ready() -> void:
-	$finish.hide()
-
-func finish_game():
-	$finish.show()
--- goof_around/wall_14.gd
+++
@@ -1,5 +0,0 @@
-extends CSGBox3D
-
-func _physics_process(delta: float) -> void:
-	rotation.y += delta * 3
-	$"../CollisionShape3D".rotation = rotation
