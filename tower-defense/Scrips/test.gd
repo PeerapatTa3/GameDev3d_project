7773737af130752e660ff8ad501886f3cf2c4de0
Soxxxer <peerapat.ta@kkumail.com>
2025-10-06 00:48:54 +0700

    placement fix

--- tower-defense/Scrips/test.gd
+++ tower-defense/Scrips/test.gd
@@ -2,9 +2,15 @@ extends Node3D
 
 @onready var enemy : PackedScene = preload("res://Mob/ufo.tscn")
 
-var enemy_to_spawn : int = 3
+var enemy_to_spawn : int = 10
 var can_spawn : bool = true
 
+func _ready() -> void:
+	var placement_ui = $UI
+	var placement_manager = $Placementmanager
+
+	# Make sure UI has the signal defined
+	placement_ui.tower_selected.connect(placement_manager.select_tower)
 
 func _process(delta):
 	game_maneger()
@@ -19,7 +25,6 @@ func game_maneger() -> void:
 		enemy_to_spawn -= 1
 		
 		can_spawn = false
-
 
 
 
