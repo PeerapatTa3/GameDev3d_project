c80f015e90aff5aaac99d52007db585c77ad5fd3
Soxxxer <peerapat.ta@kkumail.com>
2025-10-08 05:45:47 +0700

    fix

--- tower-defense/Scrips/ui.gd
+++ tower-defense/Scrips/ui.gd
@@ -20,7 +20,7 @@ func _process(delta: float) -> void:
 	$CanvasLayer/Kill.text = "Kill : " + str(GameStatus.kills)
 	$CanvasLayer/Enemy.text = "Enemy : " + str(GameStatus.enemies_remaining)
 	if GameStatus.enemies_remaining < 0:
-		$CanvasLayer/wave.text = "Enemy : 0"
+		$CanvasLayer/Enemy.text = "Enemy : 0"
 	$CanvasLayer/wave.text = "Wave " + str(GameStatus.wave)
 	$CanvasLayer/Hp_bar.value = GameStatus.hp
 
