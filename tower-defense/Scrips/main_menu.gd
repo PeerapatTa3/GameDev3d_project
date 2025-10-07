f7453e6b117b4e6501562f418ccd16594d8b2f53
Soxxxer <peerapat.ta@kkumail.com>
2025-10-08 03:56:57 +0700

    Map changing

--- tower-defense/Scrips/main_menu.gd
+++ tower-defense/Scrips/main_menu.gd
@@ -1,20 +1,23 @@
 extends Control
 
-@export var scene : PackedScene
+@onready var map1 : PackedScene = preload("res://scenes/test.tscn")
+@onready var map2 : PackedScene = preload("res://Map/grass_land.tscn")
+
 @export var scene2 : PackedScene
 
+var to_scene : PackedScene
+
 func  _ready() -> void:
+	to_scene = map1
 	AudioManager.Bg_main.play()
 	AudioManager.Bg_gameover.stop()
 	AudioManager.Bg_stage.stop()
 
-
 func _on_play_pressed() -> void:
-	get_tree().change_scene_to_packed(scene)
+	get_tree().change_scene_to_packed(to_scene)
 	AudioManager.Bg_main.stop()
 	AudioManager.Bg_stage.play()
 	
-
 
 func _on_setting_pressed() -> void:
 	#var current_scene = get_tree().current_scene
@@ -22,3 +25,12 @@ func _on_setting_pressed() -> void:
 	#get_tree().change_scene_to_file("res://scenes/setting.tscn")
 	var setting_scene = load("res://scenes/setting.tscn").instantiate()
 	get_tree().current_scene.add_child(setting_scene)
+
+func _on_option_button_item_selected(index: int) -> void:
+	match index:
+		0:
+			to_scene = map1
+		1:
+			to_scene = map2
+		_:
+			to_scene = map1
