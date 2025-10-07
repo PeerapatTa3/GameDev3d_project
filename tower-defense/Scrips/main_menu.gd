281fe8920dedecbd9fe726f93170ef183491edcd
peeky001 <peerapol123456789k@gmail.com>
2025-10-07 21:06:26 +0700

    sound

--- tower-defense/Scrips/main_menu.gd
+++ tower-defense/Scrips/main_menu.gd
@@ -3,10 +3,12 @@ extends Control
 @export var scene : PackedScene
 @export var scene2 : PackedScene
 
-
+func  _ready() -> void:
+	AudioManager.Bg_main.play()
 
 func _on_play_pressed() -> void:
 	get_tree().change_scene_to_packed(scene)
+	AudioManager.Bg_main.stop()
 
 
 func _on_setting_pressed() -> void:
